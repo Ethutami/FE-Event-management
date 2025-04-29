6e9357e9c28ca3af2eee1d5bde6b9702f9d4303b
ethutami uta <ethutami@gmail.com>
2025-04-29 21:45:08 +0800

    update responsive review card

--- src/components/reviewCard.componet.tsx
+++ src/components/reviewCard.componet.tsx
@@ -24,34 +24,65 @@ const ReviewCard = () => {
         return <div>There is no review yet</div>;
     }
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6t">
+        <div className="grid grid-cols-1 md:grid-cols-2 single-grid">
             {reviews.map((review: IReview, index: number) => {
                 const date = formatDate(review?.created_at, false, true, false)
                 const userName = review?.users?.first_name + ' ' + review?.users?.last_name
                 return (
-                    <div key={index} className="p-6 mt-10 rounded-lg shadow-md bg-white max-w-2xl mx-auto">
-                        <div className="flex items-center justify-between mb-4">
-                            <div className="flex space-x-1">
-                                {Array.from({ length: review?.rating }, (_, index) => (
-                                    <span key={index}>⭐</span>
-                                ))}
+                    <div
+                        key={index}
+                        className={`p-6 mt-8 rounded-lg shadow-md bg-white review-card ${index % 2 === 0 ? 'left-align' : 'right-align'
+                            }`}
+                    >
+                        {/* Versi untuk >=700px */}
+                        <div className="hidden md:block">
+                            <div className="flex items-center justify-between mb-4">
+                                <div className="flex space-x-1">
+                                    {Array.from({ length: review?.rating }, (_, index) => (
+                                        <span key={index}>⭐</span>
+                                    ))}
+                                </div>
+                                <span className="text-gray-500 text-sm">{date}</span>
                             </div>
+                            <p className="text-gray-700 mb-2">{review?.message}</p>
+                            <div className="flex items-center mt-6">
+                                <Image
+                                    src={review.users.profile_picture}
+                                    alt={userName}
+                                    className="w-12 h-12 rounded-full mr-4"
+                                    width={100}
+                                    height={100}
+                                />
+                                <div>
+                                    <h4 className="font-bold">{userName}</h4>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Versi untuk <700px */}
+                        <div className="block md:hidden">
+                            <div className="flex items-center mb-6">
+                                <Image
+                                    src={review.users.profile_picture}
+                                    alt={userName}
+                                    className="w-12 h-12 rounded-full mr-4"
+                                    width={100}
+                                    height={100}
+                                />
+                                <div>
+                                    <h4 className="font-bold">{userName}</h4>
+                                    <div className="flex space-x-1">
+                                        {Array.from({ length: review?.rating }, (_, index) => (
+                                            <span key={index}>⭐</span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="text-gray-700 mb-2">{review?.message}</p>
                             <span className="text-gray-500 text-sm">{date}</span>
                         </div>
-                        <p className="text-gray-700 mb-6">{review?.message}</p>
-                        <div className="flex items-center">
-                            <Image
-                                src={review.users.profile_picture}
-                                alt={userName}
-                                className="w-12 h-12 rounded-full mr-4"
-                                width={100}
-                                height={100}
-                            />
-                            <div>
-                                <h4 className="font-bold">{userName}</h4>
-                            </div>
-                        </div>
                     </div>
+
                 )
             })}
         </div>
