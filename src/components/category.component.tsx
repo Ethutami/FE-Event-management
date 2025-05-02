59b9a436446e675746ec33e402b94395cabd2ba4
ethutami uta <ethutami@gmail.com>
2025-05-02 20:53:35 +0800

    delete comment

--- src/components/category.component.tsx
+++ src/components/category.component.tsx
@@ -51,28 +51,3 @@ const CategoryChips = () => {
 };
 
 export default CategoryChips
-
-
-{/* <section className="w-full p-6">
-<h1 className="text-3xl md:text-5xl font-bold mb-6 text-center min-[700px]:text-left">Category</h1>
-<div className="hidden min-[700px]:block w-26 h-2 bg-[#0D2B50] rounded-full min-[700px]:ml-0 mx-auto"></div>
-<div className="overflow-x-auto">
-    <div className="flex gap-4 px-4 py-2 w-max">
-        {categories.map((category: ICategory, index: number) => (
-            <div
-                key={index}
-                className="flex flex-col items-center min-w-[60px] text-center m-8 max-[500px]:m-1"
-            >
-                <Image
-                    src={category.path}
-                    alt={category.category}
-                    width={40}
-                    height={40}
-                    className="custom-size object-cover flex items-center justify-center w-30 h-30 rounded-full"
-                />
-                <span className="text-sm">{category.category}</span>
-            </div>
-        ))}
-    </div>
-</div>
-</section> */}
