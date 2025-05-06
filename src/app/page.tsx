fc8c03e39810823c17182de52e305bdea20d4d66
ethutami uta <ethutami@gmail.com>
2025-05-06 20:30:31 +0800

    commit page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import LandingPage from "@/pages/landing.page";
-import LoginPage from "@/pages/login.page";
-import RegisterPage from "@/pages/register.page";
-
 
 export default function Home() {
   return (
-    // <RegisterPage />
-    // <LoginPage />
     <LandingPage />
   );
 }
