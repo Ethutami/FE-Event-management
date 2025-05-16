0bce6127224e1d2bac83aaab20073b1c27952d4e
Nahalil <nahalilmuchtar2@gmail.com>
2025-05-16 08:51:52 +0800

    fix missing reset password page

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,5 @@
+import ResetPaswordPage from "../reset-password-page/page";
+
+export default function ResetPassword() {
+    return <ResetPaswordPage/>
+}
---
+++ src/app/reset-verification/page.tsx
@@ -0,0 +1,5 @@
+import ResetVerificationPage from "../reset-verification-page/page";
+
+export default function ResetVerification() {
+    return <ResetVerificationPage/>
+}
